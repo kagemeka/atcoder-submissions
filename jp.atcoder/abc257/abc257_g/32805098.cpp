854d9e0873a985838d884816bcfdf03a09b9e860
kagemeka <kagemeka@users.noreply.github.com>
2022-06-27 18:42:46 +0000

    fetch atcoder submissions.

---
+++ jp.atcoder/abc257/abc257_g/32805098.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+namespace types {
+#include <cstdint>
+#include <functional>
+
+  using u8 = uint8_t;
+  using u16 = uint16_t;
+  using u32 = uint32_t;
+  using u64 = uint64_t;
+  using u128 = __uint128_t;
+  using i8 = int8_t;
+  using i16 = int16_t;
+  using i32 = int32_t;
+  using i64 = int64_t;
+  using i128 = __int128_t;
+  using usize = std::size_t;
+  using isize = std::make_signed<usize>::type;
+  using f32 = float;
+  using f64 = double;
+  using f128 = __float128;
+
+  template <typename T>
+  using vec = std::vector<T>;
+
+  template <typename T>
+  using fn = std::function<T>;
+
+} // namespace types
+
+namespace morris_pratt_table_1_indexed {
+  using namespace types;
+  template <typename A>
+  auto morris_pratt_table(const A& a) -> vec<int> {
+    int n = a.size();
+    vec<int> lb(n + 1, 0); // longest border
+    lb[0] = -1;
+    for (int i = 0, d = -1; i < n; ++i) {
+      while (d != -1 && a[d] != a[i]) d = lb[d];
+      ++d;
+      lb[i + 1] = d;
+    }
+    return lb;
+  }
+} // namespace morris_pratt_table_1_indexed
+auto main() -> int {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  using namespace std;
+  using namespace types;
+  string s, t;
+  cin >> s >> t;
+  usize n = s.size(), m = t.size();
+
+  using morris_pratt_table_1_indexed::morris_pratt_table;
+
+  auto lb = morris_pratt_table(s + '$' + t);
+  usize i = m, k = 0;
+  while (i > 0) {
+    usize d = lb[n + 1 + i];
+    if (!d) {
+      cout << -1 << '\n';
+      return 0;
+    }
+    i -= d;
+    ++k;
+  }
+
+  cout << k << '\n';
+}
