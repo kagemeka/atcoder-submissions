fd93579540b2366c47b0fddb15cef1bb1a768887
kagemeka <kagemeka@users.noreply.github.com>
2022-07-03 10:58:47 +0000

    fetch atcoder submissions.

---
+++ jp.atcoder/abc258/abc258_e/32964082.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class functional_graph_kth_from {
+  vector<int> path;
+  int cycle_size;
+  int cycle_start_idx;
+
+public:
+  functional_graph_kth_from(const vector<int>& f, int src) {
+    int n = f.size();
+    vector<int> order(n, -1);
+
+    for (int i = src, j = 0;; j++) {
+      if (order[i] != -1) {
+        cycle_size = j - order[i];
+        cycle_start_idx = order[i];
+        break;
+      }
+      order[i] = j;
+      path.push_back(i);
+      i = f[i];
+    }
+  }
+
+  auto operator()(long k) -> const int& {
+    if (k >= cycle_start_idx)
+      k = (k - cycle_start_idx) % cycle_size + cycle_start_idx;
+    return path[k];
+  }
+};
+
+auto main() -> int {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, q, x;
+  cin >> n >> q >> x;
+
+  vector<long> w(n);
+  for (auto& x : w) cin >> x;
+
+  long s = 0;
+  for (int i = 0; i < n; i++) {
+    s += w[i];
+    w.push_back(w[i]);
+  }
+  long p = x / s * n;
+  x %= s;
+  vector<long> cnt(n, p);
+
+  s = 0;
+  vector<int> f(n);
+  for (int i = 0, r = 0; i < n; i++) {
+    while (s < x) s += w[r++];
+    cnt[i] += r - i;
+    f[i] = r % n;
+    s -= w[i];
+  }
+
+  auto g = functional_graph_kth_from(f, 0);
+
+  while (q--) {
+    long k;
+    cin >> k;
+    cout << cnt[g(k - 1)] << endl;
+  }
+}
