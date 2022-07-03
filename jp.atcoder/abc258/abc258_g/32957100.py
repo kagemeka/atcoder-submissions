0602958d715e2e7e5016de29604b698c1e9b6fb6
kagemeka <kagemeka@users.noreply.github.com>
2022-07-03 08:13:08 +0000

    fetch atcoder submissions.

---
+++ jp.atcoder/abc258/abc258_g/32957100.py
@@ -0,0 +1,81 @@
+# / mypy: ignore-errors
+import typing
+
+
+# M0 = 0x5555555555555555
+# M1 = 0x3333333333333333
+# M2 = 0x0F0F0F0F0F0F0F0F
+# def popcount(n: int) -> int:
+#     n -= (n >> 1) & M0
+#     n = (n & M1) + ((n >> 2) & M1)
+#     n = (n + (n >> 4)) & M2
+#     n = n + (n >> 8)
+#     n = n + (n >> 16)
+#     n = n + (n >> 32)
+#     return n & 0x7F
+
+
+def popcount(n):
+    c = (n & 0x5555555555555555) + ((n >> 1) & 0x5555555555555555)
+    c = (c & 0x3333333333333333) + ((c >> 2) & 0x3333333333333333)
+    c = (c & 0x0F0F0F0F0F0F0F0F) + ((c >> 4) & 0x0F0F0F0F0F0F0F0F)
+    c = (c & 0x00FF00FF00FF00FF) + ((c >> 8) & 0x00FF00FF00FF00FF)
+    c = (c & 0x0000FFFF0000FFFF) + ((c >> 16) & 0x0000FFFF0000FFFF)
+    c = (c & 0x00000000FFFFFFFF) + ((c >> 32) & 0x00000000FFFFFFFF)
+    return c
+
+
+K = 63
+
+
+Self = typing.TypeVar("Self")
+
+
+class BitArray:
+    _d: typing.List[int]
+
+    def __init__(self, size: int) -> None:
+        self._d = [0] * ((size + M) >> K)
+
+    def __getitem__(self, i: int) -> int:
+        return self._d[i >> K] >> (i & M) & 1 == 1
+
+    def __setitem__(self, i: int, value: int) -> None:
+        if self[i] != value:
+            self.flip(i)
+
+    def flip(self, i: int) -> None:
+        self._d[i >> K] ^= 1 << (i & M)
+
+    def __and__(self, rhs: Self) -> Self:
+        res = BitArray(0)
+        res._d = self._d.copy()
+        for i in range(min(len(self._d), len(rhs._d))):
+            res._d[i] &= rhs._d[i]
+        return res
+
+    def and_count(self, rhs: Self) -> int:
+        return sum(popcount(x & y) for x, y in zip(self._d, rhs._d))
+
+    def popcount(self) -> int:
+        return sum(popcount(x) for x in self._d)
+
+
+def main() -> None:
+    n = int(input())
+    a = [BitArray(n) for _ in range(n)]
+    for i in range(n):
+        for j, v in enumerate(map(int, input())):
+            a[i][j] = v
+
+    cnt = 0
+    for i in range(n):
+        for j in range(i):
+            if not a[i][j]:
+                continue
+            # cnt += (a[i] & a[j]).popcount()
+            cnt += a[i].and_count(a[j])
+    print(cnt // 3)
+
+
+main()
---
+++ jp.atcoder/abc258/abc258_g/32957252.py
@@ -0,0 +1,52 @@
+# / mypy: ignore-errors
+import typing
+
+
+# def popcount(n: int) -> int:
+#     M0 = 0x5555555555555555
+#     M1 = 0x3333333333333333
+#     M2 = 0x0F0F0F0F0F0F0F0F
+#     n -= (n >> 1) & M0
+#     n = (n & M1) + ((n >> 2) & M1)
+#     n = (n + (n >> 4)) & M2
+#     n = n + (n >> 8)
+#     n = n + (n >> 16)
+#     n = n + (n >> 32)
+#     return n & 0x7F
+
+
+def popcount(n):
+    c = (n & 0x5555555555555555) + ((n >> 1) & 0x5555555555555555)
+    c = (c & 0x3333333333333333) + ((c >> 2) & 0x3333333333333333)
+    c = (c & 0x0F0F0F0F0F0F0F0F) + ((c >> 4) & 0x0F0F0F0F0F0F0F0F)
+    c = (c & 0x00FF00FF00FF00FF) + ((c >> 8) & 0x00FF00FF00FF00FF)
+    c = (c & 0x0000FFFF0000FFFF) + ((c >> 16) & 0x0000FFFF0000FFFF)
+    c = (c & 0x00000000FFFFFFFF) + ((c >> 32) & 0x00000000FFFFFFFF)
+    return c
+
+
+N = 63
+
+
+def main() -> None:
+    n = int(input())
+    a = []
+    for i in range(n):
+        s = input()
+        row = []
+        for j in range(0, n, N):
+            row.append(int(s[j : j + N][::-1], 2))
+        a.append(row)
+    cnt = 0
+    w = len(a[0])
+    for i in range(n):
+        for j in range(i):
+            if not a[i][j // N] >> (j % N) & 1:
+                continue
+            for k in range(w):
+                cnt += popcount(a[i][k] & a[j][k])
+            # cnt += sum(popcount(x & y) for x, y in zip(a[i], a[j]))
+    print(cnt // 3)
+
+
+main()
