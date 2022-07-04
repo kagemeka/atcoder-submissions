18fb9a9e2d1d8c3340de570b0148dc40d8aa282c
kagemeka <kagemeka@users.noreply.github.com>
2022-07-04 03:29:55 +0000

    fetch atcoder submissions.

---
+++ jp.atcoder/abc145/abc145_e/32975464.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+auto main() -> int {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, t;
+  cin >> n >> t;
+  const int K = 1 << 13;
+
+  vector<pair<int, int>> dp(K);
+  for (int i = 0; i < n; i++) {
+    int a, b;
+    cin >> a >> b;
+    for (int j = K - 1; j >= a; j--) {
+      int v = dp[j - a].first + b;
+      int w = max(dp[j - a].second, a);
+      if (v > dp[j].first) {
+        dp[j] = {v, w};
+      } else if (v == dp[j].first && w > dp[j].second) {
+        dp[j].second = w;
+      }
+    }
+  }
+
+  int mx = 0;
+  for (int i = 0; i < K; i++) {
+    auto [v, w] = dp[i];
+    if (i - w < t) mx = max(mx, v);
+  }
+  cout << mx << endl;
+}
