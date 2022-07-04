7073979ba6d6b1af92cdb7a53c45b687c247ac4f
kagemeka <kagemeka@users.noreply.github.com>
2022-07-04 23:22:12 +0000

    fetch atcoder submissions.

---
+++ jp.atcoder/abc130/abc130_e/32991221.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+auto main() -> int {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+
+  vector<int> a(n), b(m);
+  for (int i = 0; i < n; i++) cin >> a[i];
+  for (int i = 0; i < m; i++) cin >> b[i];
+
+  const int mod = 1000000007;
+  vector<int> dp(m + 1, 1);
+  for (int i = 0; i < n; i++) {
+    for (int j = m - 1; j >= 0; j--) {
+      dp[j + 1] -= dp[j] * (a[i] != b[j]);
+    }
+    for (int j = 0; j < m; j++) {
+      dp[j + 1] += dp[j];
+      dp[j + 1] %= mod;
+    }
+  }
+
+  cout << (dp[m] + mod) % mod << endl;
+}
