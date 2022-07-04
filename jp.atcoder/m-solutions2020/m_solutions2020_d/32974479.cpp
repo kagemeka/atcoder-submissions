f5d079b9e635603da53a5bba40f99fc67157aee5
kagemeka <kagemeka@users.noreply.github.com>
2022-07-04 02:39:06 +0000

    fetch atcoder submissions.

---
+++ jp.atcoder/m-solutions2020/m_solutions2020_d/32974479.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+auto main() -> int {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i = 0; i < n; i++) cin >> a[i];
+
+  long s = 1000;
+  for (int i = 0; i < n - 1; i++) {
+    if (a[i + 1] > a[i]) {
+      s += s / a[i] * (a[i + 1] - a[i]);
+    }
+  }
+  cout << s << endl;
+}
