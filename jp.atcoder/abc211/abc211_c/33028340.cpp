1d4b9949f6c00b633558b6a69bfbd00100c5436c
kagemeka <kagemeka@users.noreply.github.com>
2022-07-06 23:22:44 +0000

    fetch atcoder submissions.

---
+++ jp.atcoder/abc211/abc211_c/33028340.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+auto main() -> int {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  cin >> s;
+  int n = s.size();
+
+  string t = "chokudai";
+  int m = t.size();
+
+  int const mod = 1000000007;
+
+  vector<int> dp(m + 1);
+  dp[0] = 1;
+
+  for(int i = 0; i < n; i++) {
+    for(int j = 0; j < m; j++) {
+      if(s[i] != t[j]) continue;
+      dp[j + 1] += dp[j];
+      dp[j + 11] %= mod;
+    }
+  }
+  cout << dp[m] << endl;
+}
