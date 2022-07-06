128b9d59459c3fc7a6d380fe1f7c9e2560e4d867
kagemeka <kagemeka@users.noreply.github.com>
2022-07-06 08:52:19 +0000

    fetch atcoder submissions.

---
+++ jp.atcoder/abc137/abc137_e/33017673.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+auto main() -> int {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m, p;
+  cin >> n >> m >> p;
+
+  using tup = tuple<int, int, int>;
+  vector<tup> edges;
+  while(m--) {
+    int a, b, c;
+    cin >> a >> b >> c;
+    edges.emplace_back(--a, --b, p - c); // reverse sign
+  }
+
+  long const inf = 1l << 60;
+
+  vector<long> dist(n, inf);
+  dist[0] = 0;
+  for(int i = 0; i < n; i++) {
+    for(auto& [u, v, w]: edges) {
+      if(dist[u] == inf) continue;
+      long dv = dist[u] + w;
+      if(dv >= dist[v]) continue;
+      dist[v] = i == n - 1 ? -inf : dv;
+    }
+  }
+  assert(dist[n - 1] != inf);
+  // for(int i = 0; i < n; i++) { cout << dist[i] << " \n"[i == n - 1]; }
+  cout << (dist[n - 1] == -inf ? -1 : max(0l, -dist[n - 1])) << endl;
+}
