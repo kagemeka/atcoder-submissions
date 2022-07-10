15a7a01f8bb72b794462148351a6f22a42b7ce12
kagemeka <kagemeka@users.noreply.github.com>
2022-07-10 22:43:32 +0000

    fetch atcoder submissions.

---
+++ jp.atcoder/abc070/abc070_d/33153274.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+auto main() -> int {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n;
+  cin >> n;
+  using p = pair<int, int>;
+  vector<vector<p>> g(n);
+  for(int i = 0; i < n - 1; i++) {
+    int a, b, c;
+    cin >> a >> b >> c;
+    a--;
+    b--;
+    g[a].emplace_back(b, c);
+    g[b].emplace_back(a, c);
+  }
+  int q, k;
+  cin >> q >> k;
+  queue<int> que;
+  k--;
+  que.push(k);
+  vector<long> dist(n);
+  vector<int> parent(n, n);
+  dist[k] = 0;
+  while(que.size()) {
+    auto u = que.front();
+    que.pop();
+    for(auto& [v, w]: g[u]) {
+      if(v == parent[u]) continue;
+      parent[v] = u;
+      dist[v] = dist[u] + w;
+      que.push(v);
+    }
+  }
+  while(q--) {
+    int x, y;
+    cin >> x >> y;
+    cout << dist[--x] + dist[--y] << endl;
+  }
+}
