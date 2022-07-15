1ad8e94915c78ff796549cc47091e8329ae2257d
kagemeka <kagemeka@users.noreply.github.com>
2022-07-15 11:58:05 +0000

    fetch atcoder submissions.

---
+++ jp.atcoder/abc219/abc219_d/33240367.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+auto main() -> int {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int const inf = 1 << 30;
+  int n, x, y;
+  cin >> n >> x >> y;
+  vector<vector<int>> dp(x + 1, vector<int>(y + 1, inf));
+  dp[0][0] = 0;
+  while(n--) {
+    int a, b;
+    cin >> a >> b;
+    for(int i = x - 1; i >= 0; i--) {
+      int ni = min(x, i + a);
+      for(int j = 0; j < y; j++) {
+        int nj = min(y, j + b);
+        dp[ni][nj] = min(dp[ni][nj], dp[i][j] + 1);
+      }
+    }
+  }
+  int res = dp[x][y];
+  cout << (res == inf ? -1 : res) << endl;
+}
