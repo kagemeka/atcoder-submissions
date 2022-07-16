210669fe723356e281b756a4fffc4912b913db07
kagemeka <kagemeka@users.noreply.github.com>
2022-07-16 12:44:16 +0000

    fetch atcoder submissions.

---
+++ jp.atcoder/arc018/arc018_1/33255843.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+auto main() -> int {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  double h, b;
+  cin >> h >> b;
+  cout << setprecision(16) << h * h * b / 10000 << endl;
+}
