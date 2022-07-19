46c299681405bfc17184963e7e0d91e9d9ca41c0
kagemeka <kagemeka@users.noreply.github.com>
2022-07-19 16:25:24 +0000

    fetch atcoder submissions.

---
+++ jp.atcoder/abc042/arc058_a/33367161.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+auto main() -> int {
+  int n, m;
+  cin >> n >> m;
+  int const k = 10;
+  int s = (1 << k) - 1;
+  int lo = 0;
+  for(int i = 0; i < m; i++) {
+    int x;
+    cin >> x;
+    s &= ~(1 << x);
+    if(x == lo) lo++;
+  }
+  vector<int> next(k);
+  int x = -1;
+  for(int i = k - 1; i >= 0; i--) {
+    next[i] = x;
+    if(s >> i & 1) x = i;
+  }
+  int p = 1;
+  x = 0;
+  int mn = 0;
+  bool just = true;
+  while(n) {
+    int d = n % k;
+    n /= k;
+    mn = mn * k + lo;
+    if(just && s >> d & 1) {
+      x += p * d;
+      p *= k;
+      continue;
+    }
+    d = next[d];
+    if(d == -1) {
+      x = mn;
+      just = false;
+    } else {
+      x = mn / k + p * d;
+      just = true;
+    }
+    p *= k;
+  }
+  if(!just) x += next[0] * p;
+  cout << x << endl;
+}
---
+++ jp.atcoder/abc131/abc131_c/33367225.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+auto main() -> int {
+  long a, b, c, d;
+  cin >> a >> b >> c >> d;
+  long l = lcm(c, d);
+  auto f = [&](long n) -> long { return n - n / c - n / d + n / l; };
+  cout << f(b) - f(a - 1) << endl;
+}
