752d0ad59a943c2fd148195ed6d3dbf0e5807a87
kagemeka <kagemeka@users.noreply.github.com>
2022-07-20 10:11:34 +0000

    fetch atcoder submissions.

---
+++ jp.atcoder/arc007/arc007_2/33333881.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+auto main() -> int {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, m;
+  cin >> n >> m;
+  vector<int> cases(n + 1), cd(n + 1);
+  for(int i = 0; i < n + 1; i++) {
+    cases[i] = i;
+    cd[i] = i;
+  }
+  int x = 0;
+  while(m--) {
+    int y;
+    cin >> y;
+    swap(cd[cases[x]], cd[cases[y]]);
+    swap(cases[x], cases[y]);
+    swap(x, y);
+  }
+  for(int i = 0; i < n; i++) { cout << cd[i + 1] << endl; }
+}
