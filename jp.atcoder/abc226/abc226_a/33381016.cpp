399bf432f07dc079841d6ed558a3d5e07a7a8c7a
kagemeka <kagemeka@users.noreply.github.com>
2022-07-20 13:32:10 +0000

    fetch atcoder submissions.

---
+++ jp.atcoder/abc226/abc226_a/33381016.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+auto main() -> int {
+  double x;
+  cin >> x;
+  cout << round(x) << endl;
+}
---
+++ jp.atcoder/abc226/abc226_a/33381107.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+auto main() -> int {
+  int a, b;
+  scanf("%d.%d", &a, &b);
+  cout << a + (b >= 500) << endl;
+}
