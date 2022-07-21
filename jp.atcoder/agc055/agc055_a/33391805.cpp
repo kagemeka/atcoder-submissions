9c3ef8226f0816e6c8a802cf1a566d866253c96e
kagemeka <kagemeka@users.noreply.github.com>
2022-07-21 08:24:29 +0000

    fetch atcoder submissions.

---
+++ jp.atcoder/agc055/agc055_a/33391805.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+auto main() -> int {
+  int n;
+  string s;
+  cin >> n >> s;
+  string perm = "ABC";
+  vector<int> id(3 * n, -1);
+  int k = 0;
+  do {
+    k++;
+    int cnt = n;
+    for(int i = 0; i < 3; i++) {
+      int c = 0;
+      for(int j = i * n; j < (i + 1) * n; j++) { c += s[j] == perm[i]; }
+      cnt = min(cnt, c);
+    }
+    for(int i = 0; i < 3; i++) {
+      int c = 0;
+      for(int j = i * n; j < (i + 1) * n; j++) {
+        if(c == cnt) break;
+        if(s[j] != perm[i] || id[j] != -1) continue;
+        id[j] = k;
+        c++;
+      }
+    }
+  } while(next_permutation(perm.begin(), perm.end()));
+  for(auto& i: id) { cout << i; }
+  cout << endl;
+}
