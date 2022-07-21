e5f777d48bec5092a6f0a71fc8a59d89d2ac3e98
kagemeka <kagemeka@users.noreply.github.com>
2022-07-21 05:51:23 +0000

    fetch atcoder submissions.

---
+++ jp.atcoder/agc052/agc052_a/33389600.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+auto main() -> int {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t;
+  cin >> t;
+  while(t--) {
+    int n;
+    cin >> n;
+    vector<string> s(3);
+    for(int i = 0; i < 3; i++) cin >> s[i];
+    int idx = 0;
+    for(auto& t: s) { idx = max(idx, (int)t.find_first_of('0')); }
+    string res = "";
+    res += string(idx, '1');
+    res += '0';
+    res += string(2 * n - idx, '1');
+    cout << res << endl;
+  }
+}
