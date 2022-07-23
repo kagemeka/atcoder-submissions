b73710d559867713b6b2cff5ededc9c702a45f9e
kagemeka <kagemeka@users.noreply.github.com>
2022-07-23 03:23:18 +0000

    fetch atcoder submissions.

---
+++ jp.atcoder/arc142/arc142_b/33419566.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+auto main() -> int {
+  int n;
+  cin >> n;
+  for(int i = 0; i < n; i++) {
+    for(int j = 0; j < n; j += 2) {
+      int x = i * n + j + 1;
+      int y = i * n + j + 2;
+      if(j != n - 1) {
+        if(i & 1) swap(x, y);
+        cout << x << ' ' << y;
+        if(j < n - 2) cout << ' ';
+      } else {
+        cout << x;
+      }
+    }
+    cout << endl;
+  }
+}
