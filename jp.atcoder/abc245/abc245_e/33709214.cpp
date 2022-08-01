2b944ac762ace2eafd8ffc984f3a5048a72ee9d1
kagemeka <kagemeka@users.noreply.github.com>
2022-08-01 09:42:23 +0000

    fetch atcoder submissions.

---
+++ jp.atcoder/abc245/abc245_e/33709214.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+auto main() -> int {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, m;
+  cin >> n >> m;
+  using pi = pair<int, int>;
+  vector<pi> chocolates(n), boxes(m);
+  for(int i = 0; i < n; i++) cin >> chocolates[i].first;
+  for(int i = 0; i < n; i++) cin >> chocolates[i].second;
+  for(int i = 0; i < m; i++) cin >> boxes[i].first;
+  for(int i = 0; i < m; i++) cin >> boxes[i].second;
+  sort(chocolates.begin(), chocolates.end());
+  sort(boxes.begin(), boxes.end());
+  multiset<int> s;
+  int j = 0;
+  for(int i = 0; i < m; i++) {
+    auto [x, y] = boxes[i];
+    while(j < n && chocolates[j].first <= x) {
+      s.insert(chocolates[j].second);
+      j++;
+    }
+    auto it = s.upper_bound(y);
+    if(it == s.begin()) continue;
+    s.erase(--it);
+  }
+  cout << (s.size() || j != n ? "No" : "Yes") << endl;
+}
