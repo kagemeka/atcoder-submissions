e4f22691d81e9fe23c752b4e1fa216dec5ab63f6
kagemeka <kagemeka@users.noreply.github.com>
2022-08-10 03:40:38 +0000

    fetch atcoder submissions.

---
+++ jp.atcoder/abc108/arc102_b/33913911.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+auto main() -> int {
+  int l;
+  cin >> l;
+  using tup = tuple<int, int, int>;
+  vector<tup> res;
+  int i = 0;
+  while(l) {
+    if(l & 1) { res.emplace_back(i, -1, (l - 1) << i); }
+    l >>= 1;
+    if(l) {
+      res.emplace_back(i, i + 1, 0);
+      res.emplace_back(i, i + 1, 1 << i);
+    }
+    i++;
+  }
+  int n = i + 1;
+  cout << n << ' ' << res.size() << endl;
+  for(auto& [u, v, w]: res) {
+    if(v == -1) v = n - 1;
+    u++;
+    v++;
+    cout << u << ' ' << v << ' ' << w << endl;
+  }
+}
