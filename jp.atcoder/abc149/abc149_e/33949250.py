ef9663dd8eb5acfa4566b203aa8cda87c39a7c60
kagemeka <kagemeka@users.noreply.github.com>
2022-08-11 18:52:44 +0000

    fetch atcoder submissions.

---
+++ jp.atcoder/abc149/abc149_e/33949250.py
@@ -0,0 +1,24 @@
+# mypy: ignore-errors
+
+from scipy.signal import convolve
+import sys
+import numpy as np
+
+
+def main() -> None:
+    n, m, *a = map(int, sys.stdin.read().split())
+    a = np.array(a)
+    k = 1 << 17
+    c = np.zeros(k)
+    np.add.at(c, a, 1)
+    c = convolve(c, c).astype(np.int64)
+
+    k = c.size
+    s = (c * np.arange(k))[::-1].cumsum()
+    c = c[::-1].cumsum()
+    i = np.searchsorted(c, m)
+    res = (k - 1 - i) * (m - c[i - 1]) + s[i - 1]
+    print(res)
+
+
+main()
