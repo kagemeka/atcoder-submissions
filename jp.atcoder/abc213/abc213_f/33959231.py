064a129eda5dafd16a1733d2da0e257a98fcccca
kagemeka <kagemeka@users.noreply.github.com>
2022-08-12 09:01:16 +0000

    fetch atcoder submissions.

---
+++ jp.atcoder/abc213/abc213_f/33959231.py
@@ -0,0 +1,70 @@
+import typing
+
+
+L = typing.List
+
+
+def lcp_array(a: L[int], sa: L[int]) -> L[int]:
+    n = len(a)
+    assert n > 0
+    rank = [0] * n
+    for i, j in enumerate(sa):
+        rank[j] = i
+    lcp, h = [0] * (n - 1), 0
+    for i in range(n):
+        if h > 0:
+            h -= 1
+        r = rank[i]
+        if r == n - 1:
+            continue
+        j = sa[r + 1]
+        while max(i, j) + h < n and a[i + h] == a[j + h]:
+            h += 1
+        lcp[r] = h
+    return lcp
+
+
+# mypy: ignore-errors
+
+import numpy as np
+
+
+def suffix_array(a: np.array) -> np.array:
+    n = a.size
+    d = 1
+    while True:
+        a <<= 30
+        a[:-d] |= 1 + (a[d:] >> 30)
+        sa = a.argsort()
+        d <<= 1
+        if d >= n:
+            return sa
+        a[sa[0]], a[sa[1:]] = 0, np.cumsum(a[sa[1:]] != a[sa[:-1]])
+
+
+def main() -> None:
+    n = int(input())
+    a = [ord(x) - ord("a") for x in input()]
+    sa = suffix_array(np.array(a)).tolist()
+    lcp = lcp_array(a, sa)
+
+    a = list(range(n, 0, -1))
+    for i in range(2):
+        s = 0
+        st = []
+        for i in range(n - 1):
+            l = 1
+            h = lcp[i]
+            while st and st[-1][0] >= h:
+                x = st.pop()
+                l += x[1]
+                s -= x[0] * x[1]
+            s += h * l
+            st.append((h, l))
+            a[sa[i + 1]] += s
+        sa.reverse()
+        lcp.reverse()
+    print(*a, sep="\n")
+
+
+main()
