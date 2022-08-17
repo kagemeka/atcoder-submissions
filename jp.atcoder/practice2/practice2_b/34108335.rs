96abffcf759282d906445a473733124ce7ad0017
kagemeka <kagemeka@users.noreply.github.com>
2022-08-17 11:10:04 +0000

    fetch atcoder submissions.

---
+++ jp.atcoder/practice2/practice2_b/34108335.rs
@@ -0,0 +1,62 @@
+pub fn read<T: std::str::FromStr>() -> T {
+    use std::io::Read;
+    std::io::stdin()
+        .lock()
+        .by_ref()
+        .bytes()
+        .map(|c| c.unwrap() as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect::<String>()
+        .parse::<T>()
+        .ok()
+        .unwrap()
+}
+pub struct Fenwick(Vec<usize>);
+impl Fenwick {
+    pub fn new(size: usize) -> Self { Self(vec![0; size + 1]) }
+
+    pub fn size(&self) -> usize { self.0.len() - 1 }
+
+    pub fn add(&mut self, mut i: usize, x: usize) {
+        let n = self.size();
+        assert!(i < n);
+        i += 1;
+        while i <= n {
+            self.0[i] += x;
+            i += 1 << i.trailing_zeros();
+        }
+    }
+
+    pub fn sum_lt(&self, mut i: usize) -> usize {
+        assert!(i <= self.size());
+        let mut v = 0;
+        while i > 0 {
+            v += self.0[i];
+            i -= 1 << i.trailing_zeros();
+        }
+        v
+    }
+}
+pub fn main() {
+    use std::io::Write;
+    let out = std::io::stdout();
+    let writer = &mut std::io::BufWriter::new(out.lock());
+    let n: usize = read();
+    let q: usize = read();
+    let a: Vec<_> = (0..n).map(|_| read::<usize>()).collect();
+    let mut fw = Fenwick::new(n);
+    for i in 0..n {
+        fw.add(i, a[i]);
+    }
+    for _ in 0..q {
+        let t: u8 = read();
+        let x: usize = read();
+        let y: usize = read();
+        if t == 0 {
+            fw.add(x, y);
+        } else {
+            writeln!(writer, "{}", fw.sum_lt(y) - fw.sum_lt(x)).unwrap();
+        }
+    }
+}
