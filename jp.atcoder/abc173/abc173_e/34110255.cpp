f652a2f7a705dd3917fd3ed74cc3df6db6506207
kagemeka <kagemeka@users.noreply.github.com>
2022-08-17 12:52:40 +0000

    fetch atcoder submissions.

---
+++ jp.atcoder/abc173/abc173_e/34110255.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+auto main() -> int {
+  int n, k;
+  cin >> n >> k;
+  vector<int> neg, pos;
+  int zero = 0;
+  for(int i = 0; i < n; i++) {
+    int x;
+    cin >> x;
+    if(x == 0) {
+      zero++;
+    } else if(x < 0) {
+      neg.push_back(x);
+    } else {
+      pos.push_back(x);
+    }
+  }
+  int cn = neg.size();
+  int cp = pos.size();
+  long const mod = 1000000007;
+  int c = cp + cn / 2 * 2;
+  if(((k & 1) == 0 && c >= k) || (k & 1 && cp & 1 && c >= k)) {
+    // positive possible
+    sort(neg.begin(), neg.end());
+    sort(pos.rbegin(), pos.rend());
+    long p = 1;
+    int i = 0, j = 0;
+    while(k) {
+      long x = i < cn - 1 ? (long)neg[i] * neg[i + 1] : 0;
+      long y = j < cp - 1 ? (long)pos[j] * pos[j + 1] : 0;
+      if(x == 0 && y == 0) break;
+      if(x >= y) {
+        p *= x % mod;
+        i += 2;
+      } else {
+        j += 2;
+        p *= y % mod;
+      }
+      p %= mod;
+      k -= 2;
+    }
+    while(k) {
+      p *= pos[i];
+      p %= mod;
+      j++;
+      k--;
+    }
+    cout << p << endl;
+  } else if(zero) {
+    // zero possible
+    cout << 0 << endl;
+  } else {
+    // only negative
+    // abs greedy
+    vector<int> a;
+    for(auto& x: neg) a.push_back(x);
+    for(auto& x: pos) a.push_back(x);
+    sort(a.begin(), a.end(), [&](int x, int y) { return abs(x) < abs(y); });
+    long p = 1;
+    for(int i = 0; i < k; i++) {
+      p *= a[i];
+      p %= mod;
+    }
+    cout << (p + mod) % mod << endl;
+  }
+}
