4581aa98081363c71cd22929acabbe6290d1dd4c
kagemeka <kagemeka@users.noreply.github.com>
2022-08-30 04:43:48 +0000

    fetch atcoder submissions.

---
+++ jp.atcoder/abc227/abc227_d/34453595.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+auto main() -> int {
+  int n, k;
+  cin >> n >> k;
+  vector<long> a(n);
+  for(int i = 0; i < n; i++) cin >> a[i];
+  sort(a.begin(), a.end());
+  long s = 0;
+  int i = 0;
+  while(i < n - k + 1) {
+    s += a[i];
+    i++;
+  }
+  int c = 1;
+  while(i < n) {
+    long x = s / c;
+    if (x < a[i]) {
+      cout << x << endl;
+      return 0;
+    }
+    s += a[i];
+    i++;
+    c++;    
+  }
+  cout << s / c << endl;
+}
